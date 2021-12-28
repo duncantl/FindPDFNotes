59bd5a99b69530c98b836bb8d83e6b1ed99e7325
Duncan Temple Lang <duncan@r-project.org>
2021-12-28 09:40:37 -0800

    comments

--- R/display.R
+++ R/display.R
@@ -1,8 +1,14 @@
 display =
+    #
+    # Generic that is for displaying something but not in R
+    # Could be a PDF file in a PDF viewer/reader, something (e.g. an SVG/JavaScript-based graphic) in a Web browser.
+    # Could be a sequence of things such as pages or multiple plots in a single Web page.
+    #
 function(x, ...)
     UseMethod("display")
 
 display.PageNumbers =
+    # display the pages one after the other in the PDF viewer.    
 function(x, ...)
 {
     f = attr(x, "file") # needs to be the PDF
