881f3c579883e882c35eb74b9ecfd40c1efa2087
Duncan Temple Lang <duncan@r-project.org>
2021-12-28 08:42:32 -0800

    support specifying color and/or linewidth to narrow down the matches for <coords>.  This helps to skip <coords> that are part of figures/plots and not hand-written notes

--- R/findNotes.R
+++ R/findNotes.R
@@ -1,9 +1,22 @@
 findNotes =
-function(file, show = FALSE)    
+    #
+    # not sure the show = FALSE and the display and print method make entire sense
+    #  We can't just see the results in R by printing the object.
+    #  so probably remove the print method.
+    # and change the name show to display.
+
+    #
+    #  lwd = duncan = .5, matt = .825
+    #
+    
+function(file, color = data.frame(c('65536,0,0', '0,0,0'), c(.5, .872)), lwd = numeric(), show = FALSE)    
 {
     file = normalizePath(file)
     doc = readPDFXML(file)
-    w = getNodeSet(doc, '//page[coords]')
+
+    xpath = mkXPathQuery(color, lwd)
+
+    w = getNodeSet(doc, xpath)
     ans = structure(as.integer(sapply(w, xmlGetAttr, "number")), class = "PageNumbers", file = file)
     if(show)
         display(ans)
@@ -11,6 +24,39 @@ function(file, show = FALSE)
     invisible(ans) # don't print which will call display().
 }
 
+mkXPathQuery =
+    #
+    # mkXPathQuery()
+    # mkXPathQuery(c("red"))
+    # mkXPathQuery(c("red"), .5)
+    # mkXPathQuery(data.frame(c("red", "blue"), c(.872, .5)))
+function(color = character(), lwd = numeric(), and = is.data.frame(color))
+{
+    if((missing(color) && missing(lwd)) || (length(color) == 0 && length(lwd) == 0))
+        return('//page[coords]')
+
+    force(and)  # need to force as about to potentially change defn and type of color
+      #Example of lazy eval and debugging this.
+    
+    if(is.data.frame(color) && length(lwd) == 0) {
+        lwd = color[[2]]
+        color = color[[1]]
+    }
+
+    if(length(color) && length(lwd)) 
+        sub = paste(sprintf("(@stroke.color = '%s' %s @lineWidth = '%s')", color, if(and) "and" else "or", lwd), collapse = " or ")
+    else  if(length(color) > 0) 
+        sub = paste(sprintf("@stroke.color = '%s'", color), collapse = " or ")
+    else
+        sub = paste(sprintf("@lineWidth = '%s'", lwd), collapse = " or ")        
+
+    return(sprintf("//page[coords[%s]]", sub))    
+}
+
+
+
+
+if(FALSE)
 print.PageNumbers =
 function(x, ...  )
   display(x, ...)
