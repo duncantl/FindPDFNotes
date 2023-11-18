ad8efa433d12841dce0b29bfc2b3eae39abce302
Duncan Temple Lang <duncan@r-project.org>
2023-11-18 15:18:44 -0800

    use showPage()

--- R/display.R
+++ R/display.R
@@ -13,8 +13,9 @@ function(x, ...)
 {
     f = attr(x, "file") # needs to be the PDF
     f = gsub("xml$", "pdf", f)
+    message("q, Q or 0 will end the paging")
     for(i in seq(along = x))  {
-        skim(f, x[i])
+        showPage(p, x[i])
         if(i < length(x)) {
             cat('next page (', x[i+1], ')', sep = '')
             v = readLines(stdin(), n = 1)
